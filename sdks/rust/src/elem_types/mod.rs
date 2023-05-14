8891324d0e719ffbe5570c3f1a90acc264024e7c
Sho Nakatani <lay.sakura@gmail.com>
2023-05-14 11:39:40 +0900

    feat: ElemType no longer requires Clone

--- sdks/rust/src/elem_types/mod.rs
+++ sdks/rust/src/elem_types/mod.rs
@@ -5,14 +5,14 @@ use std::fmt;
 use crate::{coders::required_coders::Iterable, elem_types::kv::KV};
 
 /// Element types used in Beam pipelines (including PTransforms, PCollections, Coders, etc.)
-pub trait ElemType: Clone + Send + 'static {}
+pub trait ElemType: Send + Sync + 'static {}
 
 impl ElemType for Vec<u8> {}
 
 impl<K, V> ElemType for KV<K, V>
 where
-    K: Clone + Send + 'static,
-    V: Clone + Send + 'static,
+    K: Clone + Send + Sync + 'static,
+    V: Clone + Send + Sync + 'static,
 {
 }
 
@@ -32,3 +32,13 @@ impl ElemType for u64 {}
 impl ElemType for usize {}
 
 impl ElemType for () {}
+
+impl<E1: ElemType, E2: ElemType> ElemType for (E1, E2) {}
+impl<E1: ElemType, E2: ElemType, E3: ElemType> ElemType for (E1, E2, E3) {}
+impl<E1: ElemType, E2: ElemType, E3: ElemType, E4: ElemType> ElemType for (E1, E2, E3, E4) {}
+impl<E1: ElemType, E2: ElemType, E3: ElemType, E4: ElemType, E5: ElemType> ElemType
+    for (E1, E2, E3, E4, E5)
+{
+}
+
+impl<E: ElemType> ElemType for Option<E> {}
